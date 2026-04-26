210053e3e54437a222626ba260d0b54c397b396b
MohGuma <gomaam14@gamil.com>
2026-04-26 18:44:44 +0400

    style header and menu mobile button:

--- javascript/script.js
+++ javascript/script.js
@@ -10,13 +10,50 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Header scroll glass effect
+  const html = document.documentElement;
   const header = document.getElementById('site-header');
+	const hamburger = document.getElementById('hamburger');
+
+  const toggleMenu = () => {
+    // Toggle aria-expanded
+    const isExpanded =
+      hamburger.getAttribute('aria-expanded') === 'true';
+    hamburger.setAttribute('aria-expanded', !isExpanded);
+
+    // Toggle menu visibility
+    if (isExpanded) {
+      // // Hide menu
+      // siteNavigation.classList.add('max-lg:translate-x-[-150%]');
+      // siteNavigation.classList.remove('max-lg:translate-x-[0]');
+      // // Add visibility hidden after transition completes
+      // setTimeout(() => {
+      //   siteNavigation.classList.add('max-lg:invisible');
+      // }, 300); // Match the transition duration
+
+      // Remove open class from menu button
+      hamburger.classList.remove('nav-open');
+    } else {
+      // Show menu
+      // siteNavigation.classList.remove('max-lg:invisible');
+      // siteNavigation.classList.remove('max-lg:translate-x-[-150%]');
+      // siteNavigation.classList.add('max-lg:translate-x-[0]');
+
+      // Add open class to menu button
+      hamburger.classList.add('nav-open');
+    }
+
+    // Add overflow to HTML element
+    html.classList.toggle('overflow-hidden');
+
+  }
+
+  // Header scroll glass effect
   window.addEventListener('scroll', () => {
     console.log("sio")
     header.classList.toggle('header-scrolled', window.scrollY > 40);
   });
 
-
+  // Mobile menu toggle functionality
+  hamburger.addEventListener('click', toggleMenu);
 })
 
--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -8,9 +8,11 @@
 
 <header id="site-header" class="fixed top-0 left-0 right-0 z-50 h-18 transition-all duration-400">
 	<div class="container flex items-center justify-between h-full">
+
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="font-bold text-[1.35rem] tracking-[0.12em] uppercase text-white">
 			<?php bloginfo( 'name' ); ?><span class="text-primary">.</span>
 		</a>
+
 		<nav id="site-navigation" class="hidden md:block" aria-label="<?php esc_attr_e( 'Main Navigation', 'mayasarji' ); ?>">
 			<?php
 			wp_nav_menu(
@@ -23,6 +25,15 @@
 			);
 			?>
 		</nav>
-		<button id="hamburger" class="md:hidden flex flex-col gap-1.25 cursor-pointer p-1 bg-transparent border-0" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mayasarji' ); ?></button>
+
+		<button 
+			id="hamburger" 
+			class="md:hidden flex flex-col justify-center items-end gap-1.25 w-6 h-6 cursor-pointer bg-transparent border-0 outline-0 group" 
+			aria-controls="primary-menu" 
+			aria-expanded="false">
+			<span class="w-6 h-0.5 bg-white/60 group-hover:bg-white transition-all duration-300"></span>
+      <span class="w-5 h-0.5 bg-white/60 group-hover:bg-white transition-all duration-300"></span>
+      <span class="w-4 h-0.5 bg-white/60 group-hover:bg-white transition-all duration-300"></span>
+		</button>
 	</div>
 </header>
